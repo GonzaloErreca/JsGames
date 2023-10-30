6fdbe1dc31b025e5653536fc5d06db9e7692260d
Gonzalo Erreca <gonzaerreca@gmail.com>
2023-10-30 06:51:34 -0300

    numeracion del buscaminas

--- Game3/buscaminas.js
+++ Game3/buscaminas.js
@@ -3,54 +3,89 @@
 function generateGrid() {
   let mineGrid = [];
   for (let x = 0; x < 4; x++) {
-    mineGrid.push([])
+    mineGrid.push([]);
     for (let y = 0; y < 4; y++) {
       mineGrid[x][y] = Math.floor(Math.random() * 3);
-      if (mineGrid[x][y]=== 1) {
-        mineGrid[x][y] = 'X';
-      } else {
+      if (mineGrid[x][y] === 1) {
+        mineGrid[x][y] = "X";
+      } else
+      {
         mineGrid[x][y] = 0;
       }
     }
-
   }
   return mineGrid;
 }
 
-
 console.log(generateGrid());
-
+console.log(addNumbers(generateGrid()));
 //-------------------------
 
+function addNumbers(arr) {
+ 
+  const numRows = arr.length;
+  const numColumns = arr[0].length;
+ 
+   for (let x = 0; x < numRows; x++) {
+    for (let y = 0; y < numColumns; y++) {
+      //chequeamnos si tenemos que saltar a la proxima ubicacion
+      if (arr[x][y] !== "X" && arr[x][y] !== undefined){
+        let flag=0;
+        //ahora si con dos for creamos todas las posibles posiciones que rodan la ubicacion y buscamos bombas
+        for (let dx = -1; dx <= 1; dx++) {
+          for (let dy = -1; dy <= 1; dy++) {
+            const newX = x + dx;
+            const newY = y + dy;
 
-
-// function addNumbers(arr) {
+            //chequeamos que exista la posicion y la existencia de bombas y sumamos al contador flag(en la primera version eran muchos if pero no lidiaba bien con los undefined no se porque)
+            if (newX >= 0 && newX < numRows && newY >= 0 && newY < numColumns) {
+              if (arr[newX][newY] === "X") {
+              flag++;  
+              }
+            }
+          }
+        } arr[x][y] =flag;
+      }       
+    } 
+   }
    
-//     for (let x = 0; x < 4; x++) {
-      
-//       for (let y = 0; y < 4; y++) {
-//         if(arr[x][y]!=='X') {
-//             if (arr[x][y]=== 1) {
-//                 arr[x][y] = 'X';
-//             } else if{
-//               arr[x][y] = 0;
-//             } else if{
-//               arr[x][y] = 0;
-//              }else if{
-//                 arr[x][y] = 0;
-//              }else if{
-//                 arr[x][y] = 0;
-//             }else if{
-//                 arr[x][y] = 0;
-//                }
-        
-//       }
-  
-//     }
-//     return arr;
-//   }
-  
+
+   return arr;
+
+      // if (arr[x][y] !== "X") {
+      //   if (arr[x][y - 1] === "X"&& arr[x - 1][y - 1] !== undefined) {
+      //     arr[x][y] += 1;
+      //   }
+      //   if (arr[x - 1][y - 1] === "X" && arr[x - 1][y - 1] !== undefined) {
+      //     arr[x][y] += 1;
+      //   }
+      //   if (arr[x - 1][y] === "X" && arr[x - 1][y] !== undefined) {
+      //     arr[x][y] += 1;
+      //   }
+      //   if (arr[x - 1][y + 1] === "X" && arr[x - 1][y + 1] !== undefined) {
+      //     arr[x][y] += 1;
+      //   }
+      //   if (arr[x - 1][y + 1] === "X" && arr[x - 1][y + 1] !== undefined) {
+      //     arr[x][y] += 1;
+      //   }
+      //   if (arr[x][y + 1] === "X" && arr[x][y + 1] !== undefined) {
+      //     arr[x][y] += 1;
+      //   }
+      //   if (arr[x + 1][y + 1] === "X" && arr[x + 1][y + 1] !== undefined) {
+      //     arr[x][y] += 1;
+      //   }
+      //   if (arr[x + 1][y] === "X" && arr[x + 1][y] !== undefined) {
+      //     arr[x][y] += 1;
+      //   }
+      //   if (arr[x + 1][y - 1] === "X" && arr[x + 1][y - 1] !== undefined) {
+      //     arr[x][y] += 1;
+      //   }
+      // }
+    }
+
+    
+
+
 //   console.log(generateGrid());
-  
 
 //})
